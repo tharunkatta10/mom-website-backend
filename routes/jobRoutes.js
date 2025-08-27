dc7dbf974cb80e6fb2d239c3786ddfcb39f73187
akhiladharavath <106368196+akhiladharavath@users.noreply.github.com>
2025-08-27 16:30:21 +0530

    updated job controller

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// const departmentController = require("../controllers/department");
+const departmentController = require("../controllers/department");
 
-// router.post('/department', departmentController.createDepartment);
-// router.get('/departments', departmentController.getAllDepartments);
-// router.get('/department/:id', departmentController.getDepartmentById);
-// router.put('/department/:id', departmentController.updateDepartment);
-// router.delete('/department/:id', departmentController.deleteDepartment);
-// router.get('/search',departmentController.searchjobs);
+router.post('/department', departmentController.createDepartment);
+router.get('/departments', departmentController.getAllDepartments);
+router.get('/department/:id', departmentController.getDepartmentById);
+router.put('/department/:id', departmentController.updateDepartment);
+router.delete('/department/:id', departmentController.deleteDepartment);
+router.get('/search',departmentController.searchjobs);
 
-// module.exports = router;
+module.exports = router;
